559a2a73cf84d6bb206a4ef027364b7cac8be122
Dylan Thomas <dylanthomas1108@gmail.com>
2023-09-26 17:00:39 +0100

    webdev 3

--- WebDev/Year2/Sem1/Lab3/1 - Sales tax calculator/sales_tax.js
+++ WebDev/Year2/Sem1/Lab3/1 - Sales tax calculator/sales_tax.js
@@ -1,4 +1,41 @@
 "use strict";
 
-const $ = selector => document.querySelector(selector);
+const $ = (selector) => document.querySelector(selector);
 
+const handleClick = () => {
+  let subTotal = parseFloat($("#subtotal").value);
+  let taxRate = parseFloat($("#tax_rate").value);
+  console.log(`Subtotal = ${subTotal} | taxrate = ${taxRate}`);
+
+  if (subTotal < 0 || subTotal > 10000) {
+    clearBtnHandler();
+    alert("Subtotal must be between 0 and 10,000");
+    if (taxRate < 0 || taxRate > 12) {
+      clearBtnHandler();
+      alert("Tax rate must be between 0 and 12");
+    }
+  } else {
+    let salesTax = parseFloat((subTotal * (7.5 / 100)).toFixed(2));
+    console.log(salesTax);
+    $("#sales_tax").value = `${salesTax}`;
+
+    let total = parseFloat(subTotal + salesTax);
+    console.log(total);
+    $("#total").value = `${total}`;
+    $("#subtotal").focus();
+  }
+};
+
+const clearBtnHandler = () => {
+  $("#subtotal").value = "";
+  $("#tax_rate").value = "";
+  $("#sales_tax").value = "";
+  $("#total").value = "";
+  $("#subtotal").focus();
+};
+
+window.addEventListener("load", () => {
+  $("#subtotal").focus();
+  $("#calculate").addEventListener("click", handleClick);
+  $("#clear").addEventListener("click", clearBtnHandler);
+});
--- WebDev/Year2/Sem1/Lab3/2 - Income tax calculator/calculate_tax.js
+++ WebDev/Year2/Sem1/Lab3/2 - Income tax calculator/calculate_tax.js
@@ -1,8 +1,44 @@
 "use strict";
-const $ = selector => document.querySelector(selector);
+const $ = (selector) => document.querySelector(selector);
 
 document.addEventListener("DOMContentLoaded", () => {
-	// add event handlers
-	$("#calculate").addEventListener("click", processEntry);
-
+  // add event handlers
+  $("#calculate").addEventListener("click", processEntry);
+  $("#income").focus();
 });
+
+const processEntry = () => {
+  let income = parseInt($("#income").value);
+  console.log(income);
+  if (isNaN(income) || income < 0) {
+    alert("You enter enter a number greater than 0");
+    $("#income").focus();
+  } else {
+    calculateTax(income);
+    $("#income").focus();
+  }
+};
+
+const calculateTax = (income) => {
+  if (income < 9225) {
+    $("#tax").value = 0;
+  }
+  if (income >= 9225 && income < 37450) {
+    $("#tax").value = 922.5;
+  }
+  if (income >= 37450 && income < 90750) {
+    $("#tax").value = 5156.25;
+  }
+  if (income >= 90750 && income < 189300) {
+    $("#tax").value = 18481.25;
+  }
+  if (income >= 189300 && income < 411500) {
+    $("#tax").value = 46075.25;
+  }
+  if (income >= 411500 && income < 413200) {
+    $("#tax").value = 119401.25;
+  }
+  if (income >= 413200) {
+    $("#tax").value = 119996.25;
+  }
+};
