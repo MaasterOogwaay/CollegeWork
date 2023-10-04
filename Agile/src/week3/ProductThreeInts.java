6a2001b47db1d4b5dfa4ece0c87d396c429931e6
Dylan Thomas <dylanthomas1108@gmail.com>
2023-10-04 20:47:04 +0100

    agile week3

--- Agile/src/week3/ProductThreeInts.java
+++ Agile/src/week3/ProductThreeInts.java
@@ -1,6 +1,7 @@
-import java.util.Scanner;
+package week3;
 
-public class SumTwoInts {
+import java.util.Scanner;
+public class ProductThreeInts {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
@@ -8,12 +9,14 @@ public class SumTwoInts {
 		int num1 = in.nextInt();
 		System.out.print("Enter an integer: ");
 		int num2 = in.nextInt();
+		System.out.print("Enter an integer: ");
+		int num3 = in.nextInt();
 		
-		if (num1 < 1 || num2 < 1) {
+		if (num1 < 1 || num2 < 1 || num3 < 3) {
 			System.out.println("Please enter valid positive integers!");
 		} else {
-			int total = num1 + num2;
-			System.out.println("Sum = "+ total);
+			int total = num1 * num2 * num3;
+			System.out.println("Product = "+ total);
 		}
 		
 		in.close();
