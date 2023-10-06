5df5150cd291c66a2790015a6af61ebc07b22505
Dylan Thomas <dylanthomas1108@gmail.com>
2023-10-06 10:42:05 +0100

    lab 5

---
+++ WebDev/Year2/Sem1/Lab 5/1. Temp converter/convert_temp.js
@@ -0,0 +1,45 @@
+"use strict";
+const $ = (id) => document.getElementById(id);
+
+/*********************
+ *  helper functions  *
+ **********************/
+const calculateCelsius = (temp) => ((temp - 32) * 5) / 9;
+const calculateFahrenheit = (temp) => (temp * 9) / 5 + 32;
+
+const toggleDisplay = (label1Text, label2Text) => {
+  $("degree_label_1").innerHTML = label1Text;
+  $("degree_label_2").innerHTML = label2Text;
+};
+
+/****************************
+ *  event handler functions  *
+ *****************************/
+const convertTemp = () => {
+  const degreesEntered = $("degrees_entered").value;
+  console.log(degreesEntered);
+  if (isNaN(degreesEntered)) {
+    $("message").innerHTML = "You must enter a valid number of degrees";
+  } else {
+    if ($("to_celsius").checked) {
+      const conversion = Math.ceil(calculateCelsius(degreesEntered));
+      $("degrees_computed").value = conversion;
+    } else {
+      const conversion = Math.ceil(calculateFahrenheit(degreesEntered));
+      $("degrees_computed").value = conversion;
+    }
+  }
+};
+
+const toCelsius = () => toggleDisplay("Enter F degrees:", "Degrees Celsius:");
+const toFahrenheit = () => toggleDisplay("Enter C degrees:", "Degrees Fahrenheit:");
+
+document.addEventListener("DOMContentLoaded", () => {
+  // add event handlers
+  $("convert").addEventListener("click", convertTemp);
+  $("to_celsius").addEventListener("click", toCelsius);
+  $("to_fahrenheit").addEventListener("click", toFahrenheit);
+
+  // move focus
+  $("degrees_entered").focus();
+});
