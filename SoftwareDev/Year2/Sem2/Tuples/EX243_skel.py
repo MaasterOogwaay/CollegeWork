fb00b8337dc686dfe8c8bb22dd23fe756825b91f
Dylan Thomas <dylanthomas1108@gmail.com>
2024-02-06 09:22:01 +0000

    ex243 to ex246

--- SoftwareDev/Year2/Sem2/Tuples/EX243_skel.py
+++ SoftwareDev/Year2/Sem2/Tuples/EX243_skel.py
@@ -1,10 +1,10 @@
 
 def main():
-    set1= {2, 5, 2, 8, 7}
-    set2= {1,2,3,4,5,6,7}
+    set1 = {2, 5, 2, 8, 7}
+    set2 = {1, 2, 3, 4, 5, 6, 7}
     target1 = int(input('Enter Target 1: '))
     target2 = int(input('Enter Target 2: '))
-    result = inBoth(set1,set2, target1, target2)
+    result = inBoth(set1, set2, target1, target2)
     print('Element {0} & {1} found in both sets = {2}'.format(target1, target2, result))
 
 
