de5035cc291e9736c67c2baad67a993d582eb0d9
Dylan Thomas <dylanthomas1108@gmail.com>
2024-02-27 10:44:23 +0000

    pyqt5

---
+++ SoftwareDev/Year2/Sem2/PYQT5/Example/StartWindowExample.py
@@ -0,0 +1,14 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+
+from incrUI import *
+
+app = QApplication([])
+w=QMainWindow()
+w.setWindowTitle('Incrementer')
+ex = Ui_MainWindow()
+ex.setupUi(w)
+w.resize(300,300)
+w.show()
+app.exec_()
